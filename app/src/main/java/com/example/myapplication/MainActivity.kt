1ebf36921f279dcdb52ace90d7e6498bd1679007
107360128 <damondamonm7@gmail.com>
2020-11-12 19:04:18 +0800

    first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.btn_choice)
+        btn.setOnClickListener( {
+            startActivityForResult(Intent(this@MainActivity, MainActivity2::class.java), 1) })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data == null) return
+        if (requestCode == 1) {
+            if (resultCode == 101) {
+                val b = data.extras
+                val str1 = b!!.getString("drink")
+                val str2 = b.getString("sugar")
+                val str3 = b.getString("ice")
+                var tv_meal = findViewById<TextView>(R.id.tv_meal)
+                tv_meal!!.text = String.format("飲料: %s\n\n甜度: %s\n\n冰塊: %s\n\n",
+                        str1,
+                        str2,
+                        str3)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,60 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+    private var sugar = "無糖"
+    private var ice_opt = "去冰"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        var rg1 = findViewById<RadioGroup>(R.id.radioGroup)
+        rg1.setOnCheckedChangeListener( { radioGroup, i ->
+            when (i) {
+                R.id.radioButton1 -> {
+                    sugar = "無糖"
+                    sugar = "少糖"
+                    sugar = "半糖"
+                    sugar = "全糖"
+                }
+                R.id.radioButton2 -> {
+                    sugar = "少糖"
+                    sugar = "半糖"
+                    sugar = "全糖"
+                }
+                R.id.radioButton3 -> {
+                    sugar = "半糖"
+                    sugar = "全糖"
+                }
+                R.id.radioButton4 -> sugar = "全糖"
+            }
+        })
+        var rg2 = findViewById<RadioGroup>(R.id.radioGroup2)
+        rg2.setOnCheckedChangeListener( { radioGroup, i ->
+            when (i) {
+                R.id.radioButton5 -> ice_opt = "微冰"
+                R.id.radioButton6 -> ice_opt = "少冰"
+                R.id.radioButton7 -> ice_opt = "正常冰"
+            }
+        })
+        var send_btn = findViewById<Button>(R.id.btn_send)
+        send_btn.setOnClickListener(View.OnClickListener {
+            var set_drink = findViewById<EditText>(R.id.ed_drink)
+            val drink = set_drink.getText().toString()
+            val i = Intent()
+            val b = Bundle()
+            b.putString("sugar", sugar)
+            b.putString("drink", drink)
+            b.putString("ice", ice_opt)
+            i.putExtras(b)
+            setResult(101, i)
+            finish()
+        })
+    }
+}
